247efe687cec511439dc8cf87041001d01e0b646
Kiryl Yermalovich <ts.underawater@gmail.com>
2022-04-05 20:38:42 +0300

    add: added hadle edit

--- src/components/Todo/Todo.tsx
+++ src/components/Todo/Todo.tsx
@@ -1,21 +1,24 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { ITodo } from '../../types/ITodo';
 import { AiFillEdit, AiFillDelete } from 'react-icons/ai';
 import { MdDone } from 'react-icons/md';
 import './styles.css';
 
 interface TodoProps {
-    todo: ITodo;
-    setTodos: (todo: ITodo[]) => void;
-    todos: ITodo[];
+  todo: ITodo;
+  setTodos: (todo: ITodo[]) => void;
+  todos: ITodo[];
 }
 
-const Todo: React.FC<TodoProps> = ({ todo, todos, setTodos}) => {
+const Todo: React.FC<TodoProps> = ({ todo, todos, setTodos }) => {
+  const [edit, setEdit] = useState<boolean>(false);
+  const [editText, setEditText] = useState<string>(todo.todo);
+  const inputRef = useRef<HTMLInputElement>(null)
 
   const isTodoDone = (id: number) => {
     setTodos(todos.map(todo => {
-      if(todo.id === id) {
-        return {...todo, isDone: !todo.isDone}
+      if (todo.id === id) {
+        return { ...todo, isDone: !todo.isDone }
       } else {
         return todo
       }
@@ -26,17 +29,48 @@ const Todo: React.FC<TodoProps> = ({ todo, todos, setTodos}) => {
     setTodos(todos.filter(todo => todo.id !== id))
   }
 
+  const handleEdit = (e: React.FormEvent<HTMLFormElement>, id: number) => {
+    e.preventDefault();
+    setTodos(todos.map(todo => {
+      if (todo.id === id) {
+        return { ...todo, todo: editText }
+      } else {
+        return todo
+      }
+    }));
+    setEdit(false);
+  }
+
+  useEffect(() => {
+    inputRef.current?.focus()
+  }, [edit])
+
   return (
-    <div className='tasklist__todo'>
-        <span className={`tasklist__todo-item ${todo.isDone ? 'isDone' : ''}`}>
+    <form className='tasklist__todo' onSubmit={(e) => handleEdit(e, todo.id)}>
+      {
+        edit ? (
+          <input
+            ref={inputRef}
+            value={editText}
+            onChange={(e) => setEditText(e.target.value)}
+            className='tasklist__todo-input'
+          />
+        ) : (
+          <p className={`tasklist__todo-item ${todo.isDone ? 'isDone' : ''}`}>
             {todo.todo}
-        </span>
-        <div className='tasklist__todo-icons'>
-            <AiFillEdit />
-            <AiFillDelete onClick={() => deleteTodo(todo.id)} />
-            <MdDone onClick={() => isTodoDone(todo.id)} />
-        </div>
-    </div>
+          </p>
+        )
+      }
+      <div className='tasklist__todo-icons'>
+        <AiFillEdit onClick={() => {
+          if (!edit && !todo.isDone) {
+            setEdit(!edit)
+          }
+        }} />
+        <AiFillDelete onClick={() => deleteTodo(todo.id)} />
+        <MdDone onClick={() => isTodoDone(todo.id)} />
+      </div>
+    </form>
   )
 }
 
